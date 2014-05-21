a925f9c8a5e0e85ef55bf44edd928ab6c409f36b
Glen <glen.arrowsmith@gmail.com>
2014-05-21 14:34:09 +1000

    Done

--- README.md
+++ README.md
@@ -2,3 +2,75 @@ gulp-headerfooter
 =================
 
 A gulp plugin for adding headers and footers to your files
+
+
+Install
+-------
+
+Install with [npm](https://npmjs.org/package/gulp-headerfooter).
+
+```
+npm install --save-dev gulp-headerfooter
+```
+
+Examples
+--------
+
+Filenames as the header & footer
+
+```js
+var gulp = require('gulp');
+var headerfooter = require('gulp-headerfooter');
+
+gulp.task('default', function () {
+	gulp.src('./app/content')
+		.pipe(headerfooter.header('./app/partials/header.html'));
+		.pipe(headerfooter.footer('./app/partials/footer.html'));
+        .pipe(gulp.dest('./public/'))
+});
+```
+
+Strings as the header & footer
+
+```js
+var gulp = require('gulp');
+var headerfooter = require('gulp-headerfooter');
+
+gulp.task('default', function () {
+    gulp.src('./app/content')
+        .pipe(headerfooter.header('<html><body>'));
+        .pipe(headerfooter.footer('</body></html>'));
+        .pipe(gulp.dest('./public/'))
+});
+```
+
+Buffers as the header & footer
+
+```js
+var gulp = require('gulp');
+var headerfooter = require('gulp-headerfooter');
+var fs = require('fs');
+
+var header = fs.readFileSync('./app/partials/header.html');
+var footer = fs.readFileSync('./app/partials/footer.html');
+
+gulp.task('default', function () {
+    gulp.src('./app/content')
+        .pipe(headerfooter.header(header));
+        .pipe(headerfooter.footer(footer));
+        .pipe(gulp.dest('./public/'))
+});
+```
+
+Short hand version
+
+```js
+var gulp = require('gulp');
+var headerfooter = require('gulp-headerfooter');
+
+gulp.task('default', function () {
+    gulp.src('./app/content')
+        .pipe(headerfooter('<html><body>', '</body></html>');
+        .pipe(gulp.dest('./public/'))
+});
+```
---
+++ gulp-headerfooter.js
@@ -0,0 +1,86 @@
+var through = require('through2');
+var fs = require('fs');
+
+module.exports = function(header, footer) {
+    // return through.obj(headerfooter);
+    return through.obj(function(file, encoding, next) {
+        headerfooter(file, header, footer, encoding, next);
+    });
+};
+
+module.exports.header = function(header) {
+    return through.obj(function(file, encoding, next) {
+        headerfooter(file, header, null, encoding, next);
+    });
+};
+
+module.exports.footer = function(footer) {
+    return through.obj(function(file, encoding, next) {
+        headerfooter(file, null, footer, encoding, next);
+    });
+};
+
+
+
+
+function headerfooter(file, header, footer, encoding, next) {
+    console.log("headerfooter");
+
+    var bothDone = function() {
+        done(file, header, footer, next);
+    }
+
+    var callbackCount = 0;
+    checkType(header, function(err, input) {
+        if (err) { return next(err); }
+
+        header = input;
+        if (++callbackCount == 2) {
+            bothDone();
+        }
+    });
+
+    checkType(footer, function(err, input) {
+        if (err) { return next(err); }
+
+        footer = input;
+        if (++callbackCount == 2) {
+            bothDone();
+        }
+    });
+
+}
+
+function done(file, header, footer, next) {
+    var contents = [];
+
+    if (header)         { contents.push(header); }
+    if (file.contents)  { contents.push(file.contents); }
+    if (file.footer)    { contents.push(file.footer); }
+
+    file.contents = Buffer.concat(
+        contents
+    );
+    next(null, file);
+}
+
+function checkType(input, done) {
+    if (input instanceof Buffer) {
+        done(null, input);
+    } else if (typeof input == "string") {
+        //Could be path or full string
+        fs.readFile(input, function(err, fileBuffer) {
+            if (err) {
+                //Probably doesn't exist so its a string.
+                done(null, new Buffer(input));
+            } else {
+                input = fileBuffer;
+                done(null, fileBuffer);
+            }
+        });
+    } else if (input == null) {
+        done(null, null);
+    } else {
+        done(new Error("Unknown type. Either buffer, filename or string accepted."));
+    }
+}
